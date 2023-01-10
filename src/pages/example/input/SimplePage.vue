e03993f02218bd3e50363782008add1721bf0b15
leo <leo@leos-MacBook-Pro.local>
2023-01-10 12:38:37 +0800

    update

--- src/pages/example/input/SimplePage.vue
+++ src/pages/example/input/SimplePage.vue
@@ -18,14 +18,14 @@ const breadcrumb_list = [
     <div class="mt-5 grid grid-cols-12">
       <div class="box col-span-12 lg:col-span-6">
         <label for="email">Email1</label>
-        <input type="email" name="email" id="email" class="mt-1" placeholder="you@example.com" />
+        <input type="email" name="email" id="email" class="rounded mt-1" placeholder="you@example.com" />
       </div>
     </div>
 
     <div class="mt-5 grid grid-cols-12">
       <div class="box col-span-12 lg:col-span-6">
         <label for="email">Email2</label>
-        <input type="email" name="email" id="email" class="mt-1" placeholder="you@example.com" />
+        <input type="email" name="email" id="email" class="rounded mt-1" placeholder="you@example.com" />
         <p class="mt-2">We'll only use this for spam.</p>
       </div>
     </div>
@@ -36,14 +36,14 @@ const breadcrumb_list = [
           <label for="email">Email3</label>
           <p id="email-optional">Optional</p>
         </div>
-        <input type="email" name="email" id="email" class="mt-1" placeholder="you@example.com" />
+        <input type="email" name="email" id="email" class="rounded mt-1" placeholder="you@example.com" />
       </div>
     </div>
 
     <div class="mt-5 grid grid-cols-12">
       <div class="box col-span-12 lg:col-span-6">
         <label for="email">Email4</label>
-        <input type="email" name="email" id="email" value="you@example.com" disabled="" class="disabled mt-1" placeholder="you@example.com" />
+        <input type="email" name="email" id="email" value="you@example.com" disabled="" class="rounded disabled mt-1" placeholder="you@example.com" />
       </div>
     </div>
 
@@ -54,7 +54,7 @@ const breadcrumb_list = [
           <div class="prefix">
             <EnvelopeIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
           </div>
-          <input type="email" name="email" id="email" class="pl-10" placeholder="you@example.com" />
+          <input type="email" name="email" id="email" class="rounded pl-10" placeholder="you@example.com" />
         </div>
       </div>
     </div>
@@ -63,7 +63,7 @@ const breadcrumb_list = [
       <div class="box col-span-12 lg:col-span-6">
         <label>Account number</label>
         <div class="input-wrap mt-1">
-          <input type="text" name="account-number" id="account-number" placeholder="000-00-0000" />
+          <input type="text" name="account-number" id="account-number" class="rounded" placeholder="000-00-0000" />
           <div class="suffix">
             <QuestionMarkCircleIcon class="h-5 w-5" />
           </div>
@@ -76,7 +76,7 @@ const breadcrumb_list = [
         <label for="company-website" class="block text-sm font-medium text-gray-700">Company Website</label>
         <div class="input-wrap mt-1">
           <div class="prefix">http://</div>
-          <input type="text" name="company-website" id="company-website" class="pl-14" placeholder="www.example.com" />
+          <input type="text" name="company-website" id="company-website" class="rounded pl-14" placeholder="www.example.com" />
         </div>
       </div>
     </div>
@@ -86,7 +86,7 @@ const breadcrumb_list = [
         <label for="price">Price</label>
         <div class="input-wrap mt-1">
           <div class="prefix">$</div>
-          <input type="text" name="price" id="price" class="pl-7 pr-12" placeholder="0.00" />
+          <input type="text" name="price" id="price" class="rounded pl-7 pr-12" placeholder="0.00" />
           <div class="suffix">USD</div>
         </div>
       </div>
@@ -103,7 +103,7 @@ const breadcrumb_list = [
               <option>EU</option>
             </select>
           </div>
-          <input type="text" name="phone-number" id="phone-number" class="pl-18" placeholder="+1 (555) 987-6543" />
+          <input type="text" name="phone-number" id="phone-number" class="rounded pl-18" placeholder="+1 (555) 987-6543" />
         </div>
       </div>
     </div>
@@ -113,7 +113,7 @@ const breadcrumb_list = [
         <label for="price">Price</label>
         <div class="input-wrap">
           <span class="prefix">$</span>
-          <input type="text" name="price" id="price" class="pl-7 pr-12" placeholder="0.00" />
+          <input type="text" name="price" id="price" class="rounded pl-7 pr-12" placeholder="0.00" />
           <div class="suffix">
             <select id="currency" name="currency">
               <option>USD</option>
@@ -129,7 +129,7 @@ const breadcrumb_list = [
       <div class="box col-span-12 lg:col-span-6">
         <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
         <div class="mt-1 input-wrap">
-          <input type="text" name="name" id="name" class="bg-gray-50" placeholder="Jane Smith" />
+          <input type="text" name="name" id="name" class="rounded bg-gray-50" placeholder="Jane Smith" />
         </div>
       </div>
     </div>
@@ -138,7 +138,7 @@ const breadcrumb_list = [
       <div class="box col-span-12 lg:col-span-6">
         <label>Quick search</label>
         <div class="input-wrap">
-          <input type="text" name="search" id="search" />
+          <input type="text" class="rounded" name="search" id="search" />
           <div class="suffix">
             <kbd>⌘K</kbd>
           </div>
@@ -150,9 +150,9 @@ const breadcrumb_list = [
       <div class="box col-span-12 lg:col-span-6">
         <label for="search">Multi-compressed form</label>
         <div class="-space-y-px rounded-md shadow-sm">
-          <input id="email-address" name="email" type="email" autocomplete="email" class="relative rounded-b-none" placeholder="Email address" />
-          <input id="email-address" name="email" type="text" required="" class="relative rounded-none" placeholder="mid address" />
-          <input id="password" name="password" type="password" autocomplete="current-password" class="relative rounded-md rounded-t-none" placeholder="Password" />
+          <input id="email-address" name="email" type="email" autocomplete="email" class="relative rounded-t" placeholder="Email address" />
+          <input id="email-address" name="email" type="text" required="" class="relative" placeholder="mid address" />
+          <input id="password" name="password" type="password" autocomplete="current-password" class="relative rounded-b" placeholder="Password" />
         </div>
       </div>
     </div>
@@ -161,17 +161,17 @@ const breadcrumb_list = [
       <div class="box col-span-12 lg:col-span-6">
         <legend>Card Details</legend>
         <div class="mt-1 -space-y-px">
-          <input type="text" name="card-number" id="card-number" class="relative rounded-b-none" placeholder="Card number" />
+          <input type="text" name="card-number" id="card-number" class="relative rounded-t" placeholder="Card number" />
 
           <div class="flex -space-x-px">
-            <input type="text" name="card-expiration-date" id="card-expiration-date" class="w-1/2 relative rounded-none rounded-bl-md" placeholder="MM / YY" />
-            <input type="text" name="card-cvc" id="card-cvc" class="w-1/2 relative rounded-none rounded-br-md" placeholder="CVC" />
+            <input type="text" name="card-expiration-date" id="card-expiration-date" class="w-1/2 relative rounded-bl" placeholder="MM / YY" />
+            <input type="text" name="card-cvc" id="card-cvc" class="w-1/2 relative rounded-br" placeholder="CVC" />
           </div>
         </div>
 
         <legend class="mt-3">Billing address</legend>
         <div class="mt-1 -space-y-px">
-          <select id="country" name="country" autocomplete="country-name" class="relative rounded-none rounded-t-md">
+          <select id="country" name="country" autocomplete="country-name" class="relative rounded-t">
             <option>United States</option>
             <option>Canada</option>
             <option>Mexico</option>
