373437314e1738f14d1316d51bbfc954671a6f66
leo <leo@leos-MacBook-Pro.local>
2023-01-18 11:58:32 +0800

    update

--- src/components/core/modal/Modal.vue
+++ src/components/core/modal/Modal.vue
@@ -3,10 +3,12 @@ import { ref, toRef, watch, defineEmits } from "vue";
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from "@headlessui/vue";
 import { XMarkIcon } from "@heroicons/vue/24/outline";
 
-const props = defineProps(["open"]);
+const props = defineProps(["open", "disableClose"]);
 const emit = defineEmits(["update:open"]);
 function setopen() {
-  emit("update:open", false);
+  if (!props.disableClose) {
+    emit("update:open", false);
+  }
 }
 </script>
 
@@ -18,19 +20,18 @@ function setopen() {
       </TransitionChild>
 
       <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div class="flex min-h-full justify-center p-4 text-center items-center sm:p-0">
           <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-              <div class="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
+            <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all xl:max-w-7xl my-8">
+              <div class="absolute top-0 right-0 pt-4 pr-4" v-if="!props.disableClose">
                 <button type="button" class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" @click="setopen">
-                  <span class="sr-only">Close</span>
-                  <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                  <XMarkIcon class="h-6 w-6" />
                 </button>
               </div>
-              <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <slot></slot>
+              <div class="bg-white px-8 pt-5 pb-4">
+                <slot name="body"></slot>
               </div>
-              <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <div class="bg-gray-50 px-4 py-3 flex-row-reverse flex">
                 <slot name="footer"></slot>
               </div>
             </DialogPanel>
--- src/pages/example/modal/ModalSimplePage.vue
+++ src/pages/example/modal/ModalSimplePage.vue
@@ -12,38 +12,27 @@ watch(open, (new_open) => {
   console.log("modal new open :" + new_open);
 });
 
-function setopen(){
-  open.value=true
+function setopen() {
+  open.value = true;
 }
-
 </script>
 
 <template>
   <SidebarLayout>
-
     <button class="btn-primary" @click="setopen">Open Modal Window</button>
 
-    <Modal v-model:open="open">
-      <template v-slot:default>
-        <div>
-          <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-            <CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
-          </div>
-          <div class="mt-3 text-center sm:mt-5">
-            <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">Payment Successful xxx</DialogTitle>
-            <div class="mt-2">
-              <p class="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.</p>
-            </div>
-          </div>
-        </div>
-        <div class="mt-5 sm:mt-6">
-          <button type="button" class=" w-full justify-center w-full btn-primary " @click="open = false">Go back to dashboard</button>
+    <Modal v-model:open="open" :disable-close="true">
+      <template v-slot:body>
+        
+        <div class="mt-5">
+          <label>A disable close style modal</label>
+          <p> you can only click buttons below to close modal window </p>
         </div>
       </template>
 
       <template v-slot:footer>
-        <button type="button" class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm" @click="open = false">Deactivate</button>
-        <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" @click="open = false" ref="cancelButtonRef">Cancel</button>
+        <button type="button" class="btn-err mr-3" @click="open = false">Deactivate</button>
+        <button type="button" class="btn-secondary mr-3" @click="open = false">Cancel</button>
       </template>
     </Modal>
   </SidebarLayout>
