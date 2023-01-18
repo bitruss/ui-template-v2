d3b22ad57e1b0a127324e5ecc0fb29a3308a6a09
leo <leo@leos-MacBook-Pro.local>
2023-01-18 12:16:47 +0800

    update

--- src/pages/example/modal/ModalSimplePage.vue
+++ src/pages/example/modal/ModalSimplePage.vue
@@ -1,39 +1,69 @@
 <script setup>
 import SidebarLayout from "../../../layouts/sidebar/SidebarLayout.vue";
 
-import { DialogTitle } from "@headlessui/vue";
 import { ref, watch } from "vue";
 import Modal from "../../../components/core/modal/Modal.vue";
-import { CheckIcon } from "@heroicons/vue/24/outline";
 
-const open = ref(true);
-
-watch(open, (new_open) => {
-  console.log("modal new open :" + new_open);
-});
-
-function setopen() {
-  open.value = true;
-}
+const open = ref(false);
+const open_withclose = ref(false);
 </script>
 
 <template>
   <SidebarLayout>
-    <button class="btn-primary" @click="setopen">Open Modal Window</button>
+    <div class="space-y-8 divide-y divide-gray-200 divide-gray-200">
+      <div>
+        <h1 class="text-2xl leading-6">Examples of Modal</h1>
+        <p class="mt-3">Different types of modal</p>
+      </div>
 
-    <Modal v-model:open="open" :disable-close="true">
-      <template v-slot:body>
-        
-        <div class="mt-5">
-          <label>A disable close style modal</label>
-          <p> you can only click buttons below to close modal window </p>
+      <div class="pt-5 lg:grid lg:grid-cols-3 lg:gap-4">
+        <div>
+          <label for="username" class="flex">Modal with close</label>
+          <p>A modal window with margin-click close</p>
+          <p>and right top close btn</p>
         </div>
-      </template>
+        <div class="lg:col-span-2 mt-2">
+          <button class="btn-primary" @click="open_withclose = true">Open Closable Modal Window</button>
+          <Modal v-model:open="open_withclose">
+            <template v-slot:body>
+              <div class="mt-5">
+                <label>A disable close style modal</label>
+                <p>you can only click buttons below to close modal window</p>
+              </div>
+            </template>
+            <template v-slot:footer>
+              <button type="button" class="btn-err mr-3" @click="open_withclose = false">Deactivate</button>
+              <button type="button" class="btn-secondary mr-3" @click="open_withclose = false">Cancel</button>
+            </template>
+          </Modal>
+        </div>
+      </div>
 
-      <template v-slot:footer>
-        <button type="button" class="btn-err mr-3" @click="open = false">Deactivate</button>
-        <button type="button" class="btn-secondary mr-3" @click="open = false">Cancel</button>
-      </template>
-    </Modal>
+
+      <div class="pt-5 lg:grid lg:grid-cols-3 lg:gap-4">
+        <div>
+          <label for="username" class="flex">Modal without close</label>
+          <p>A modal window without margin-click close</p>
+          <p>and right top close btn</p>
+        </div>
+        <div class="lg:col-span-2 mt-2">
+          <button class="btn-primary" @click="open = true">Open Modal Window</button>
+          <Modal v-model:open="open" :disable-close="true">
+            <template v-slot:body>
+              <div class="mt-5">
+                <label>A disable close style modal</label>
+                <p>you can only click buttons below to close modal window</p>
+              </div>
+            </template>
+            <template v-slot:footer>
+              <button type="button" class="btn-err mr-3" @click="open = false">Deactivate</button>
+              <button type="button" class="btn-secondary mr-3" @click="open = false">Cancel</button>
+            </template>
+          </Modal>
+        </div>
+      </div>
+
+
+    </div>
   </SidebarLayout>
 </template>
